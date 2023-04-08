f6383198f487807a822c9471ccb986096485e720
Fahri Akbaba <fahriakbaba@gmail.com>
2023-04-08 18:23:59 +0300

    e-commerce project was started

---
+++ app.js
@@ -0,0 +1,52 @@
+// to create a virtual data
+const data = [
+    {
+        title: "Diriliş",
+        author: "Tolstoy",
+        year: 1899,
+        url:""
+    },
+    {
+        title: "Sefiller",
+        author: "Victor Hugo",
+        year: 1862,
+        url:""
+    },
+    {
+        title: "Bir İdam Mahkumunun Son Günü",
+        author: "Victor Hugo",
+        year: 1829,
+        url:""
+    },
+    {
+        title: "Ana",
+        author: "Maksim Gorki",
+        year: 1906,
+        url:""
+    },
+    {
+        title: "Markopaşa Yazıları ve Ötekiler",
+        author: "Sbahatti Ali",
+        year: 1998,
+        url:"https://www.yapikrediyayinlari.com.tr/dosyalar/2020/06/Markopasa_yazilari-2230.jpg"
+    }
+];
+
+//to get elements
+const shoppingDiv = document.querySelector(".container");
+
+
+
+const showDataOnBrowser = data.map(item => {
+    return `
+     <div>
+        <img src="${item.url}" alt="cart-image">
+        <h4>${item.title}</h4>
+        <p>${item.author}</p>
+     </div>
+    `;
+});
+
+
+shoppingDiv.innerHTML = showDataOnBrowser.join("");
+ 
