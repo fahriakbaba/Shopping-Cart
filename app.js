e428d236a4f78b0821edc6473cd1a491141f8701
Fahri Akbaba <fahriakbaba@gmail.com>
2023-04-09 11:03:13 +0300

    index.html was finished and was styled by using sass

--- app.js
+++ app.js
@@ -4,25 +4,55 @@ const data = [
         title: "Diriliş",
         author: "Tolstoy",
         year: 1899,
-        url:""
+        url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XeEtDf8086y3W9rAD9oxlBM_aI1v6j2iIw&usqp=CAU"
     },
     {
         title: "Sefiller",
         author: "Victor Hugo",
         year: 1862,
-        url:""
+        url:"https://cdn.akakce.com/-/sefiller-victor-hugo-z.jpg"
     },
     {
         title: "Bir İdam Mahkumunun Son Günü",
         author: "Victor Hugo",
         year: 1829,
-        url:""
+        url:"https://productimages.hepsiburada.net/s/36/500/10531608920114.jpg"
     },
     {
         title: "Ana",
         author: "Maksim Gorki",
         year: 1906,
-        url:""
+        url:"https://img.kitapyurdu.com/v1/getImage/fn:11432909/wh:true/miw:200/mih:200"
+    },
+    {
+        title: "Markopaşa Yazıları ve Ötekiler",
+        author: "Sbahatti Ali",
+        year: 1998,
+        url:"https://www.yapikrediyayinlari.com.tr/dosyalar/2020/06/Markopasa_yazilari-2230.jpg"
+    },
+    {
+        title: "Diriliş",
+        author: "Tolstoy",
+        year: 1899,
+        url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9XeEtDf8086y3W9rAD9oxlBM_aI1v6j2iIw&usqp=CAU"
+    },
+    {
+        title: "Sefiller",
+        author: "Victor Hugo",
+        year: 1862,
+        url:"https://cdn.akakce.com/-/sefiller-victor-hugo-z.jpg"
+    },
+    {
+        title: "Bir İdam Mahkumunun Son Günü",
+        author: "Victor Hugo",
+        year: 1829,
+        url:"https://productimages.hepsiburada.net/s/36/500/10531608920114.jpg"
+    },
+    {
+        title: "Ana",
+        author: "Maksim Gorki",
+        year: 1906,
+        url:"https://img.kitapyurdu.com/v1/getImage/fn:11432909/wh:true/miw:200/mih:200"
     },
     {
         title: "Markopaşa Yazıları ve Ötekiler",
@@ -44,7 +74,7 @@ shoppingIcon.addEventListener("click", () => location.href = "cart.html")
 const showDataOnBrowser = data.map(item => {
  
     return `
-     <div>
+     <div class="cart">
         <img src="${item.url}" alt="cart-image" width="40px">
         <h4>${item.title}</h4>
         <p>${item.author}</p>
